9dcabea1d41469c3d4cb06a2009f7e28cff01d29
damlatsr <damlatsr@gmail.com>
2019-12-28 15:55:19 +0000

    Updates

---
+++ precache-manifest.a0cba6c61b5432c7cfc18c75f32bbcc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61673f489e0063ae9bad1da49204ca40",
+    "url": "/tv-show/index.html"
+  },
+  {
+    "revision": "77080a47ebe5ea52be8c",
+    "url": "/tv-show/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "21f26c100356bc4fdfff",
+    "url": "/tv-show/static/css/main.17e2b190.chunk.css"
+  },
+  {
+    "revision": "77080a47ebe5ea52be8c",
+    "url": "/tv-show/static/js/2.f87020f1.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/tv-show/static/js/2.f87020f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21f26c100356bc4fdfff",
+    "url": "/tv-show/static/js/main.6f9d51dd.chunk.js"
+  },
+  {
+    "revision": "f562cc9948a54bdc678c",
+    "url": "/tv-show/static/js/runtime-main.bd4f9f4a.js"
+  }
+]);
