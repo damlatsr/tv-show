c8e77e267436da3cbf5f0dda0a5f05791dccbb67
Damla Tasar <damlatsr@gmail.com>
2020-01-02 14:24:07 +0300

    es2016

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,26 +1,26 @@
 // actions oluşturuldu.
-export const APPEND_NEW_RESULTS = 'APPEND_NEW_RESULTS'
-
-
+export const APPEND_NEW_RESULTS = "APPEND_NEW_RESULTS"; 
 
 //tetikleniyor ve dispatch ediliyor.
+
 export const receiveNewSearch = searchText => {
-    console.log({a:searchText})
-    
-    return dispatch => {
-        return fetch(`https://api.tvmaze.com/search/shows?q=${searchText}`)
-        .then((resp) => resp.json())
-        .then(function(data) {
-          dispatch(appendNewResults(data)) 
-    })
-}
-}
+  console.log({
+    a: searchText
+  });
+  return dispatch => {
+    return fetch(`https://api.tvmaze.com/search/shows?q=${searchText}`)
+      .then(resp => resp.json())
+      .then(function(data) {
+        dispatch(appendNewResults(data));
+      });
+  };
+}; 
 
 //actions creator
-export const appendNewResults = results =>  ({
-    type: APPEND_NEW_RESULTS,
-    results: results
-})
 
+export const appendNewResults = results => ({
+  type: APPEND_NEW_RESULTS,
+  results: results
+}); 
 
 //reducere gidecek
