9b4db517219fd0a1848add83811c17a1c3de49db
Damla Tasar <damlatsr@gmail.com>
2020-01-02 14:13:24 +0300

    es2016

--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -1,87 +1,163 @@
-import React from 'react'
-import {Container, Row, Col, Card, ListGroup} from 'react-bootstrap'
-import Navbar from '../components/Navbar'
-import Loading from '../components/Loading'
-
+import React from "react";
+import { Container, Row, Col, Card, ListGroup } from "react-bootstrap";
+import Navbar from "../components/Navbar";
+import Loading from "../components/Loading";
 
 class ShowPage extends React.Component {
-   constructor(props) {
-     super(props);
-     this.state = { 
-       show:null,
-      };
-   }
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: null
+    };
+  }
 
   componentDidMount() {
-    const self = this
+    const self = this;
     fetch(`https://api.tvmaze.com/shows/${this.props.match.params.id}`)
-    .then((resp) => resp.json())
-    .then(function(data) {
-      self.setState({
-        show: data
-      })
-    })
+      .then(resp => resp.json())
+      .then(function(data) {
+        self.setState({
+          show: data
+        });
+      });
   }
 
   render() {
-    return (
-      this.state.show ?
-      
-        <div className="show-detail">
-         <Navbar title={this.state.show.name}></Navbar>
-          <Container className="container">
-            <h1 className="my-4">{this.state.show.name}
-              <sub><span className="text-muted"> {this.state.show.status}</span></sub>
-            </h1>
-
-            <Row>
-
+    return this.state.show
+      ? React.createElement(
+          "div",
+          {
+            className: "show-detail"
+          },
+          React.createElement(Navbar, {
+            title: this.state.show.name
+          }),
+          React.createElement(
+            Container,
+            {
+              className: "container"
+            },
+            React.createElement(
+              "h1",
               {
-                this.state.show.image ?
-                <Col md={8}>
-                  <Card.Img className="img-fluid" src={this.state.show.image.original} alt={this.state.show.name} />
-                </Col> : null
-              }
-
-              <Col md={4}>
+                className: "my-4"
+              },
+              this.state.show.name,
+              React.createElement(
+                "sub",
+                null,
+                React.createElement(
+                  "span",
+                  {
+                    className: "text-muted"
+                  },
+                  " ",
+                  this.state.show.status
+                )
+              )
+            ),
+            React.createElement(
+              Row,
+              null,
+              this.state.show.image
+                ? React.createElement(
+                    Col,
+                    {
+                      md: 8
+                    },
+                    React.createElement(Card.Img, {
+                      className: "img-fluid",
+                      src: this.state.show.image.original,
+                      alt: this.state.show.name
+                    })
+                  )
+                : null,
+              React.createElement(
+                Col,
                 {
-                  this.state.show.genres ?
-                  this.state.show.genres.map((genre, index) => {
-                    return (<span key={index} className="badge badge-primary m-1">{genre}</span>);
-                  }):null
-                }
-                <Card.Title className="my-3">Description</Card.Title>
-                <Card.Text dangerouslySetInnerHTML={{__html: this.state.show.summary}}></Card.Text>
-                <Card.Title className="my-3">Schedule</Card.Title>
-              <ListGroup variant="flush">
-                  <ListGroup.Item disabled>{
-                  this.state.show.schedule.time ?
-                  this.state.show.schedule.time
-                  :<span>time information not found...</span>
-                }</ListGroup.Item>
-                  <ListGroup.Item disabled>{
-                  this.state.show.schedule.days != "" ?
-                  this.state.show.schedule.days
-                  :<span>day information not found...</span>
-                }</ListGroup.Item>
-                 <ListGroup.Item disabled>{
-
-                  this.state.show.rating.average ?
-                  this.state.show.rating.average
-                  :<span>average information not found...</span>
-                }</ListGroup.Item>
-              </ListGroup>
-              </Col>
-
-            </Row>
-
-          </Container>
-        </div>
-      :
-          <Loading />
-        
-    );
+                  md: 4
+                },
+                this.state.show.genres
+                  ? this.state.show.genres.map((genre, index) => {
+                      return React.createElement(
+                        "span",
+                        {
+                          key: index,
+                          className: "badge badge-primary m-1"
+                        },
+                        genre
+                      );
+                    })
+                  : null,
+                React.createElement(
+                  Card.Title,
+                  {
+                    className: "my-3"
+                  },
+                  "Description"
+                ),
+                React.createElement(Card.Text, {
+                  dangerouslySetInnerHTML: {
+                    __html: this.state.show.summary
+                  }
+                }),
+                React.createElement(
+                  Card.Title,
+                  {
+                    className: "my-3"
+                  },
+                  "Schedule"
+                ),
+                React.createElement(
+                  ListGroup,
+                  {
+                    variant: "flush"
+                  },
+                  React.createElement(
+                    ListGroup.Item,
+                    {
+                      disabled: true
+                    },
+                    this.state.show.schedule.time
+                      ? this.state.show.schedule.time
+                      : React.createElement(
+                          "span",
+                          null,
+                          "time information not found..."
+                        )
+                  ),
+                  React.createElement(
+                    ListGroup.Item,
+                    {
+                      disabled: true
+                    },
+                    this.state.show.schedule.days != ""
+                      ? this.state.show.schedule.days
+                      : React.createElement(
+                          "span",
+                          null,
+                          "day information not found..."
+                        )
+                  ),
+                  React.createElement(
+                    ListGroup.Item,
+                    {
+                      disabled: true
+                    },
+                    this.state.show.rating.average
+                      ? this.state.show.rating.average
+                      : React.createElement(
+                          "span",
+                          null,
+                          "average information not found..."
+                        )
+                  )
+                )
+              )
+            )
+          )
+        )
+      : React.createElement(Loading, null);
   }
 }
 
