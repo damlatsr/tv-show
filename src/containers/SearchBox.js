5db6519c54574bb508ac1a025745f5b2f8043d6e
Damla Tasar <damlatsr@gmail.com>
2020-01-02 13:57:31 +0300

    es2016

--- src/containers/SearchBox.js
+++ src/containers/SearchBox.js
@@ -1,27 +1,41 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import { receiveNewSearch } from '../actions';
-import {withRouter} from 'react-router-dom'
-import { Form, Col } from 'react-bootstrap';
-import { Row } from 'react-bootstrap';
+import React from "react";
+import { connect } from "react-redux";
+import { receiveNewSearch } from "../actions";
+import { withRouter } from "react-router-dom";
+import { Form, Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 
 class SearchBox extends React.Component {
   constructor(props) {
     super(props);
-    props.dispatch(receiveNewSearch({
-      searchMessage: 'batman'
-    }))
+    props.dispatch(
+      receiveNewSearch({
+        searchMessage: "batman"
+      })
+    );
   }
 
   render() {
-    return (
-      <Form>
-        <Row>
-          <Col md={6}>
-            <input type="search" value={this.state.searchMessage} onChange={this.onSearchChangeHandler} placeholder="Search" ref="search" />
-          </Col>
-        </Row>
-      </Form>
+    return React.createElement(
+      Form,
+      null,
+      React.createElement(
+        Row,
+        null,
+        React.createElement(
+          Col,
+          {
+            md: 6
+          },
+          React.createElement("input", {
+            type: "search",
+            value: this.state.searchMessage,
+            onChange: this.onSearchChangeHandler,
+            placeholder: "Search",
+            ref: "search"
+          })
+        )
+      )
     );
   }
 }
