e751866fdbecda3c51be087df0b2a0a12dfe9674
Damla Tasar <damlatsr@gmail.com>
2020-01-02 14:08:06 +0300

    es2016

--- src/containers/ShowBoxContainer.js
+++ src/containers/ShowBoxContainer.js
@@ -1,44 +1,76 @@
-import React from 'react'
-import ShowBox from '../components/ShowBox'
-import * as actionCreators from "../actions/index"
-import { connect } from 'react-redux';
-import Loading from '../components/Loading';
-import Navbar from '../components/Navbar'
-import {withRouter} from 'react-router-dom'
-import { Container, Col, CardColumns, Row } from 'react-bootstrap';
+import React from "react";
+import ShowBox from "../components/ShowBox";
+import * as actionCreators from "../actions/index";
+import { connect } from "react-redux";
+import Loading from "../components/Loading";
+import Navbar from "../components/Navbar";
+import { Container, Col, CardColumns, Row } from "react-bootstrap";
 
-class ShowBoxContainer extends React.Component {
 
-  componentDidMount() {
-    this.props.receiveNewSearch(this.props.searchText)
-  }
-
-  render() {
-    return (
-<Container fluid>
-    <Navbar />
-      <Row className="row m-3">
-         
-        <Col>
-          <CardColumns>
-          {
-            this.props.requests.results.length > 0 ?
-            this.props.requests.results.map((show, index) => {
-              return <ShowBox {...show} key={index} />;
-            }) :
-            <Loading />
+function _extends() {
+  _extends =
+    Object.assign ||
+    function(target) {
+      for (var i = 1; i < arguments.length; i++) {
+        var source = arguments[i];
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
           }
-          </CardColumns>
-        </Col>
-      </Row>
-      </Container>
-    )
-  }
+        }
+      }
+      return target;
+    };
+  return _extends.apply(this, arguments);
 }
 
 
-const mapStateToProps=(state)=>{
-  return state
+
+class ShowBoxContainer extends React.Component {
+  componentDidMount() {
+    this.props.receiveNewSearch(this.props.searchText);
+  }
+
+  render() {
+    return React.createElement(
+      Container,
+      {
+        fluid: true
+      },
+      React.createElement(Navbar, null),
+      React.createElement(
+        Row,
+        {
+          className: "row m-3"
+        },
+        React.createElement(
+          Col,
+          null,
+          React.createElement(
+            CardColumns,
+            null,
+            this.props.requests.results.length > 0
+              ? this.props.requests.results.map((show, index) => {
+                  return React.createElement(
+                    ShowBox,
+                    _extends({}, show, {
+                      key: index
+                    })
+                  );
+                })
+              : React.createElement(Loading, null)
+          )
+        )
+      )
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return state;
 };
 
-export default connect(mapStateToProps, actionCreators)(ShowBoxContainer)
+export default connect(
+  mapStateToProps,
+  actionCreators
+)(ShowBoxContainer);
